6ca81e15089848c1423b847cc437c3654dea36c4
Roberto Aranovich <roberto.aranovich@gmail.com>
2019-09-15 19:45:06 -0400

    Add files via upload

---
+++ src/main/java/edu/RobertoAranovich/advancedjava/StockServiceType.java
@@ -0,0 +1,11 @@
+package edu.RobertoAranovich.advancedjava;
+
+/**
+ * Enumeration of StockServiceType (to be extended).
+ *
+ * @author Roberto Aranovich
+ */
+
+public enum StockServiceType {
+    BASIC
+}
