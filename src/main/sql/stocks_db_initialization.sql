b4e33dde084ebecbb5de5e6791e571a758540992
Roberto Aranovich <roberto.aranovich@gmail.com>
2019-10-11 21:09:52 -0400

    Update stocks_db_initialization.sql

--- src/main/sql/stocks_db_initialization.sql
+++ src/main/sql/stocks_db_initialization.sql
@@ -3,7 +3,7 @@
 USE stocks;
 
 DROP TABLE IF EXISTS quotes CASCADE;
-CREATE TABLE quotes(
+CREATE TABLE stocks.quotes(
    id INT NOT NULL AUTO_INCREMENT,
    symbol VARCHAR(4) NOT NULL,
    time DATETIME NOT NULL,
