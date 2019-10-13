02770cb5179264632310cb6e8f006b5a77e23b9c
Roberto Aranovich <roberto.aranovich@gmail.com>
2019-10-13 19:25:29 -0400

    Update DatabaseUtils.java

--- src/main/java/com/origamisoftware/teach/advanced/util/DatabaseUtils.java
+++ src/main/java/com/origamisoftware/teach/advanced/util/DatabaseUtils.java
@@ -14,6 +14,9 @@ import java.sql.SQLException;
  * A class that contains database related utility methods.
  */
 public class DatabaseUtils {
+    
+    public static final String initializationFile = "./src/main/sql/stocks_db_initialization.sql" ;
+
 
     // in a real program these values would be a configurable property and not hard coded.
     // JDBC driver name and database URL
