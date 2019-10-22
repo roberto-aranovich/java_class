198bba5c66bc97ebd0b3ebdded9ed4ec26804701
Roberto Aranovich <roberto.aranovich@gmail.com>
2019-10-21 20:38:29 -0400

    Update StockServiceFactoryTest.java

--- src/test/java/com/origamisoftware/teach/advanced/services/StockServiceFactoryTest.java
+++ src/test/java/com/origamisoftware/teach/advanced/services/StockServiceFactoryTest.java
@@ -2,12 +2,22 @@ package com.origamisoftware.teach.advanced.services;
 
 import org.junit.Test;
 
+import org.junit.Before;
+
 import static org.junit.Assert.assertNotNull;
+
+import com.origamisoftware.teach.advanced.util.DatabaseUtils;
+
 
 /**
  * JUnit test for <CODE>StockServiceFactory</CODE>
  */
 public class StockServiceFactoryTest {
+    
+    @Before
+    public void setup() throws Exception {
+        DatabaseUtils.initializeDatabase(DatabaseUtils.initializationFile);
+    }
 
     @Test
     public void testGetInstance() {
